46b221510b49fe81ff3ad43df801a31c2c3eb0de
Nicolas Bañales Trivelli <ninotrivelli@gmail.com>
2021-10-30 20:40:22 -0300

    hamburger links disappear when clicked

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,16 @@
 let hamburgerMenu = document.getElementById('hamburgerMenu');
 let header = document.querySelector('header');
 let body = document.querySelector('body');
+let hamburgerLinks = document.querySelectorAll('.hamburger-links');
+
 hamburgerMenu.addEventListener('click', () => {
   header.classList.toggle('open');
   body.classList.toggle('noscroll');
+});
+
+hamburgerLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    header.classList.toggle('open');
+    body.classList.toggle('noscroll');
+  });
 });
